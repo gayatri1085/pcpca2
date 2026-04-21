ff2761c9907b4a574de63d529ac4f8566d1cebee
gayatri rayasam <e0123053@sriher.edu.in>
2026-04-21 09:44:55 +0530

    Update ordersReducer.js

--- src/reducers/ordersReducer.js
+++ src/reducers/ordersReducer.js
@@ -1,22 +1,26 @@
-// src/reducers/ordersReducer.js
-
-export const initialState = {
-  orders: [],
-  loading: false,
-  error: null,
-  filterQuery: ""
-};
-
 export function ordersReducer(state, action) {
   switch (action.type) {
     case "FETCH_START":
       return { ...state, loading: true, error: null };
+
     case "FETCH_SUCCESS":
       return { ...state, loading: false, orders: action.payload };
+
     case "FETCH_ERROR":
       return { ...state, loading: false, error: action.payload };
+
     case "SET_FILTER":
       return { ...state, filterQuery: action.payload };
+    case "UPDATE_ORDER_STATUS":
+      return {
+        ...state,
+        orders: state.orders.map(order =>
+          order.orderId === action.payload.id
+            ? { ...order, status: action.payload.status }
+            : order
+        )
+      };
+
     default:
       return state;
   }
